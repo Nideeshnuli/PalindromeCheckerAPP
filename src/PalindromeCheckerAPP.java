d1c337bb7de334d9008d9771886c7d18d7f1567c
Nideeshnuli <nideeshnuli@gmail.com>
2026-02-16 11:05:08 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerAPP.java
@@ -0,0 +1,14 @@
+/*
+* @author Nideesh
+* @Version 1.0
+*
+*/
+public class PalindromeCheckerAPP {
+    public static void main (String[] args){
+
+        System.out.println("Welcome to Palindrome Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System Initialized Successfully");
+
+    }
+}
