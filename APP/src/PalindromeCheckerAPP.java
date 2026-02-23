5bfe60077c879f03d32f2c33325a2731d50f49dc
Nideeshnuli <nideeshnuli@gmail.com>
2026-02-23 10:32:27 +0530

    Nideeshnuli Added UC3-Sting

--- APP/src/PalindromeCheckerAPP.java
+++ APP/src/PalindromeCheckerAPP.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class PalindromeCheckerAPP {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+        if (str.equals(rev)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+        sc.close();
+    }
 }
